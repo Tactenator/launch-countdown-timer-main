a6d1d7c2d6fa9ceec056b341fbb51fdcfeb54ae7
Tactenator <trevor.mclaurine@gmail.com>
2023-10-10 22:18:53 -0500

    finishing styles; adding fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,15 +2,21 @@
 module.exports = {
   content: ["./**.{html,js}"],
   theme: {
-    extend: {},
-    backgroundColor: {
-      "desat-blue": 'hsl(236, 21%, 26%)', 
-      "gray-blue": 'hsl(240, 21%, 22%)'
-    }, 
-    colors: {
-      'soft-red': 'hsl(345, 95%, 68%)', 
-      'white': '#fff'
-    }
+    extend: {
+      backgroundColor: {
+        "desat-blue": 'hsl(236, 21%, 26%)', 
+        "gray-blue": 'hsl(240, 21%, 22%)', 
+        "black": "#000"
+      }, 
+      colors: {
+        'soft-red': 'hsl(345, 95%, 68%)', 
+        'white': '#fff', 
+        'dark-blue': 'hsl(235, 16%, 14%)'
+      },
+      fontFamily: {
+        "red-hat": ["red-hat", "sans-serif"]
+      }
+    },
   },
   plugins: [],
 }
